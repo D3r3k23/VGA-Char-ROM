275880e9820c98fd5c10193b1d9eb0115078aeba
Derek Henderson <62639977+D3r3k23@users.noreply.github.com>
2020-12-16 23:47:05 -0800

    Create main.c

---
+++ test_application/main.c
@@ -0,0 +1,55 @@
+
+#include "char_rom.h"
+
+extern Color color;
+
+
+void delay(int x)
+    { for (int i = 0; i < x * 20000000; i++); }
+
+
+int main(void)
+{
+    init();
+
+    delay(40);
+
+    set_background_color(color.black);
+    set_char_color(color.white);
+
+    set_char_pos(632, 352); // Center of screen
+    display_char('E');
+
+    delay(10);
+
+    clear_char();
+    set_background_color(color.white);
+    set_char_color(color.blue);
+    delay(4);
+    while(1)
+    {
+        set_char_pos(588, 352);
+        display_char('V');
+        delay(1);
+        set_char_pos(604, 352);
+        display_char('E');
+        delay(1);
+        set_char_pos(616, 352);
+        display_char('R');
+        delay(1);
+        set_char_pos(632, 352);
+        display_char('I');
+        delay(1);
+        set_char_pos(648, 352);
+        display_char('L');
+        delay(1);
+        set_char_pos(664, 352);
+        display_char('O');
+        delay(1);
+        set_char_pos(680, 352);
+        display_char('G');
+        delay(1);
+    }
+
+    return 0;
+}
