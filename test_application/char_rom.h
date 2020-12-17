044ab668f80cd9c935698992c243bed8940a5d9b
Derek Henderson <62639977+D3r3k23@users.noreply.github.com>
2020-12-17 00:00:02 -0800

    Update char_rom.h

--- test_application/char_rom.h
+++ test_application/char_rom.h
@@ -12,31 +12,33 @@
 #define CHAR_SET   *(( uint32_t *) 0x43C0000C)
 
 
-typedef struct Color_t
-{
-    uint32_t black;
-    uint32_t white;
-    uint32_t red;
-    uint32_t blue;
-    uint32_t green;
-} Color;
+typedef uint32_t Color;
 
-Color color;
+typedef struct Colors_t
+{
+    Color BLACK;
+    Color WHITE;
+    Color RED;
+    Color GREEN;
+    Color BLUE;
+} Colors;
+
+Colors colors;
 
 
 void clear_char(void);
 
 void init(void);
 
-void set_background_color(uint32_t color);
+void set_background_color(Color color);
 
-void set_char_color(uint32_t color);
+void set_char_color(Color color);
 
 void set_char_pos(uint16_t x, uint16_t y);
 
 void display_char(char ch);
 
-uint32_t create_color(uint8_t R, uint8_t G, uint8_t B);
+Color create_color(uint8_t R, uint8_t G, uint8_t B);
 
 
 #endif // CHAR_ROM_H
